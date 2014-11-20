ac1fb3b50705e60ecb05b40e79ea006af7982e3a
Radamanthus Batnag <rad@batnag.org>
2014-11-21 07:07:29 +0800

    Increment the progress bar every 1 second

--- app/screens/splash_screen.rb
+++ app/screens/splash_screen.rb
@@ -1,17 +1,18 @@
 class SplashScreen < PM::Screen
   title "Screen"
 
-  def on_appear
-    # This isn't working. The progress bar is updated after we exit this method
-    # See
-    # http://stackoverflow.com/questions/1503761/what-is-the-most-robust-way-to-force-a-uiview-to-redraw
-    # http://stackoverflow.com/questions/1455878/modifying-an-iphone-animation-container-view-before-starting-animation/1501650#1501650
-    @progress_bar = rmq(UIProgressView).get
-    5.times do
-      sleep 1
-      @progress_bar.setProgress(@progress_bar.progress + 0.2)
-      @progress_bar.setNeedsDisplay
+  def increment_progress
+    progress_bar = rmq(UIProgressView).get
+    progress_bar.setProgress(progress_bar.progress + 0.2)
+    puts "Updating progress to #{progress_bar.progress}"
+    progress_bar.setNeedsDisplay
+    if progress_bar.progress < 1.0
+      self.performSelector("increment_progress", withObject: nil, afterDelay: 1.0)
     end
+  end
+
+  def on_appear
+    self.performSelector("increment_progress", withObject: nil, afterDelay: 1.0)
   end
 
   def on_load
